3b891ecb50211a6802c7a96f08e63725a78dccfe
Sarthak Shrivastava <161567581+SarthakShrivastav-a@users.noreply.github.com>
2026-01-30 10:52:05 +0530

    chore: added jwt provider and configured filter

--- src/main/java/api/assignment/backend/repository/UserRepository.java
+++ src/main/java/api/assignment/backend/repository/UserRepository.java
@@ -3,5 +3,9 @@ package api.assignment.backend.repository;
 import api.assignment.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
